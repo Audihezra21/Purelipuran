db7c802b40e40697d580f9b1a8b504b4eeb18ec9
syifarizki <rizkisyifa379@gmail.com>
2024-06-14 17:47:17 +0800

    Workbox

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,2 +1,7 @@
 import './components/index';
 import '../../public/styles/style.css';
+import swRegister from './utils/sw-register';
+
+window.addEventListener('load', () => {
+  swRegister();
+});
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,7 @@
 import 'regenerator-runtime';
 import '../../public/styles/style.css';
-import './components/index';
+import swRegister from './utils/sw-register';
+
+window.addEventListener('load', () => {
+  swRegister();
+});
